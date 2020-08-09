052466a3601c32806b96f918f91a2e7832fe682d
鲱鱼 <lf18234129242@163.com>
2020-08-09 21:30:40 +0800

    webpack联系

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html'
+    // })
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  mode: 'development',
+  // mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      // {
+      //   test: /.(png|jpg|gif|jpeg)$/,
+      //   use: 'file-loader'
+      // },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10240 // 如果图片大小小于10k，自动打包成base64
+          }
+        }]
+      }
+    ]
+  }
+}
