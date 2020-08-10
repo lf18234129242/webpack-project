f5068a6a31244980e9b9b495179908e81c8759e8
鲱鱼 <lf18234129242@163.com>
2020-08-10 09:41:20 +0800

    文件指纹添加

---
+++ webpack.prod.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]_[chunkhash:8].js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      // {
+      //   test: /.(png|jpg|gif|jpeg)$/,
+      //   use: [{
+      //     loader: 'url-loader',
+      //     options: {
+      //       limit: 10240 // 如果图片大小小于10k，自动打包成base64
+      //     }
+      //   }]
+      // }
+    ]
+  },
+  plugins: [
+    // 把css提取成单独的文件
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano')
+    })
+  ]
+}
